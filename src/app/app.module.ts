6d17266567d7e239dd445de2b8d3197588b72f13
Alanfg17 <alanferg123@gmail.com>
2021-11-27 22:15:39 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeroesModule } from './Heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
+    //componentes
     AppComponent
   ],
+  //importacion de otros modulos
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
+  //modulo principal
   bootstrap: [AppComponent]
 })
 export class AppModule { }
